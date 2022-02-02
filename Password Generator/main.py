e5a8dfef2c0d8147051a6d676c16f2c9ee825359
HunterDragonWolf <88144273+HunterDragonWolf@users.noreply.github.com>
2022-02-02 22:38:05 +0530

    Add files via upload

---
+++ Password Generator/main.py
@@ -0,0 +1,28 @@
+import string
+import random
+
+if __name__ == "__main__":
+    s1 = string.ascii_lowercase
+    # print(s1)
+    s2 = string.ascii_uppercase
+    # print(s2)
+    s3 = string.digits
+    # print(s3)
+    s4 = string.punctuation
+    # print(s4)
+    while True:
+        try:
+            passwordlen = int(input("Enter the password length = "))
+            break
+        except ValueError:
+            print("Oops! That's not the valid number. Please try again...")
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    # print(s)
+    random.shuffle(s)
+    # print(s)
+    print("Your Password is")
+    print("".join(s[0:passwordlen]))
